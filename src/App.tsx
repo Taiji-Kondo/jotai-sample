deedcb66ccbfe6ff71bd98eda8f4a11ab3dc7db6
taiji <k.taiji1108@gmail.com>
2022-11-07 19:41:18 +0900

    feat: Create Provider sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ReactNode} from 'react';
 import './App.css';
+import {countState} from "./atoms";
+import {Provider, useAtom, useAtomValue} from "jotai";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Provider>
+          <ProviderComponent >
+            <ProviderNestComponent />
+          </ProviderComponent>
+        </Provider>
+        <ProviderLessComponent />
+        <ProviderLessBrotherComponent />
       </header>
     </div>
   );
+}
+
+const ProviderLessComponent = () => {
+  const [count, setCount] = useAtom(countState);
+
+  return (
+    <div>
+      <h1>Provider Less</h1>
+      <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+      <span>{count}</span>
+      <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+    </div>
+  );
+}
+
+const ProviderLessBrotherComponent = () => {
+  const count = useAtomValue(countState);
+
+  return (
+    <div>
+      <h1>Provider Less Brother</h1>
+      <div>{count}</div>
+    </div>
+  )
+}
+
+const ProviderComponent = ({children}: {children: ReactNode}) => {
+  const [count, setCount] = useAtom(countState);
+
+  return (
+    <div>
+      <h1>Provider</h1>
+      <button onClick={() => setCount((prev) => prev + 1)}>+</button>
+      <span>{count}</span>
+      <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+      {children}
+    </div>
+  );
+}
+
+const ProviderNestComponent = () => {
+  const count = useAtomValue(countState);
+
+  return (
+    <div>
+      <h1>Provider Nest</h1>
+      <div>{count}</div>
+    </div>
+  )
 }
 
 export default App;
